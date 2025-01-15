2d1ee027184827f91e14b7609fdeb0511d5ce095
RAJA KUMAR PASWAN <rajakumarpaswan505@gmail.com>
2025-01-16 00:03:59 +0530

    fix:route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
+
 import ToDoDashboard from './pages/ToDoDashboard';
 // import ListOfWors from './components/ListOfWors';
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<ToDoDashboard />} />
+      
+      <Route path="/" element={<ToDoDashboard />} />
  
 
       
